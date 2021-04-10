455fd331def4efb216eb2c9517002fe64ffda8e7
Jasper B <xjet16@gmail.com>
2021-04-10 23:21:49 +0800

    Fixed single post template

--- classes/BlogPost.php
+++ classes/BlogPost.php
@@ -5,6 +5,7 @@
 
 		private $category;
 		private $author;
+		public $is_comments_allowed;
 
 
 
@@ -12,7 +13,7 @@
 
 			
 			if($rows = $GLOBALS['db']->get_data("SELECT bp.id, bp.title, bp.post_img, bp.content, bpc.category_name,
-														u.firstname, u.lastname, bp.date_posted
+														u.firstname, u.lastname, bp.date_posted, bp.comments_allowed
 												 FROM blog_posts as bp
 												 INNER JOIN blog_post_categories as bpc
 												 ON bp.category_id = bpc.id
@@ -40,6 +41,42 @@
 
 
 		}
+
+
+
+		public static function fetch_one($id) {
+
+			if($rows = $GLOBALS['db']->get_data("SELECT bp.id, bp.title, bp.post_img, bp.content, bpc.category_name,
+														u.firstname, u.lastname, bp.date_posted, bp.comments_allowed
+												 FROM blog_posts as bp
+												 INNER JOIN blog_post_categories as bpc
+												 ON bp.category_id = bpc.id
+												 INNER JOIN users as u
+												 ON bp.author_id = u.id
+												 WHERE bp.id = $id")) {
+				foreach ($rows as $row) {
+
+
+					$this_object              		  = new self;
+					$this_object->id          		  = $row['id'];
+					$this_object->title       		  = $row['title'];
+					$this_object->image       		  = $row['post_img'];
+					$this_object->text        		  = $row['content'];
+					$this_object->category    		  = $row['category_name'];
+					$this_object->author      		  = $row['firstname'] . " " . $row['lastname'];
+					$this_object->date_posted 		  = $row['date_posted'];
+					$this_object->is_comments_allowed = $row['comments_allowed'] ? true : false;
+					$result                	  		  = $this_object;
+
+				}
+
+				return $result;
+
+			} else
+				return false;
+
+		}
+
 
 
 		public static function fetch_categories() {
--- post.php
+++ post.php
@@ -5,15 +5,13 @@
     include 'classes/autoloader.php';
     include 'inc/connection.php';
     include 'inc/user_init.php';
-    $page_title = "Home";
+    
 
 
-    $slides =          new Content();
-    $blog_posts =      new BlogPost();
-    $portfolios =      new Portfolio();
-    $slide_items =     $slides->fetch_all();
-    $blog_post_items = $blog_posts->fetch_all();
-    $portfolio_items = $portfolios->fetch_all();
+    $post = BlogPost::fetch_one($_GET['id']);
+    $page_title = $post->get_title();
+
+
 
 ?>
 
@@ -61,14 +59,14 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Blog Post
-                    <small>by <a href="#">Start Bootstrap</a>
+                <h1 class="page-header"><?php echo $post->get_title(); ?>
+                    <small>by <a href="#"><?php echo $post->get_author(); ?></a>
                     </small>
                 </h1>
                 <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
+                    <li><a href="/">Home</a>
                     </li>
-                    <li class="active">Blog Post</li>
+                    <li class="active"><?php echo $post->get_title(); ?></li>
                 </ol>
             </div>
         </div>
@@ -85,79 +83,66 @@
                 <hr>
 
                 <!-- Date/Time -->
-                <p><i class="fa fa-clock-o"></i> Posted on August 24, 2013 at 9:00 PM</p>
+                <p><i class="fa fa-clock-o"></i> Posted on <?php echo $post->get_date_posted(); ?></p>
 
                 <hr>
 
                 <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="/oop-cms/uploads/<?php echo $post->get_image(); ?>" alt="">
 
                 <hr>
 
                 <!-- Post Content -->
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+                <p><?php echo $post->get_content(); ?></p>
 
-                <hr>
 
-                <!-- Blog Comments -->
+                <?php if($post->is_comments_allowed) : ?>
 
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+                    <hr>
 
-                <hr>
+                    <!-- Blog Comments -->
 
-                <!-- Posted Comments -->
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <!-- Comments Form -->
+                    <div class="well">
+                        <h4>Leave a Comment:</h4>
+                        <form role="form">
+                            <div class="form-group">
+                                <textarea class="form-control" rows="3"></textarea>
                             </div>
-                        </div>
-                        <!-- End Nested Comment -->
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                     </div>
-                </div>
+
+                    <!-- Posted Comments -->
+
+
+                    <!-- 
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Start Bootstrap
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            
+                            <div class="media">
+                                <a class="pull-left" href="#">
+                                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading">Nested Start Bootstrap
+                                        <small>August 25, 2014 at 9:30 PM</small>
+                                    </h4>
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div> -->
+
+                <?php endif; ?>
 
             </div>
 
@@ -177,7 +162,7 @@
                 </div>
 
                 <!-- Blog Categories Well -->
-                <div class="well">
+                <!-- <div class="well">
                     <h4>Blog Categories</h4>
                     <div class="row">
                         <div class="col-lg-6">
@@ -205,8 +190,7 @@
                             </ul>
                         </div>
                     </div>
-                    <!-- /.row -->
-                </div>
+                </div> -->
 
                 <!-- Side Widget Well -->
                 <div class="well">
